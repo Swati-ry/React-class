ae96e7378a5c5e2960f20955415245c47c3d805f
Swati Yadav <swatiyadav0303@gmail.com>
2024-09-20 18:22:00 +0530

    first-commit

---
+++ first-project-1/src/App.js
@@ -0,0 +1,15 @@
+import Home from "./Pages/home"
+import Profile from "./Pages/profile";
+
+const App = () =>{
+  return (
+    <div className="App">
+     
+   <Home/>
+   <Profile/>
+
+    </div>
+  );
+}
+
+export default App;
